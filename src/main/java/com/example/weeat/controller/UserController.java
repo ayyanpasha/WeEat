b0326b3926e94120faf42c28f3780db40f59d4c1
ayyanpasha <ayyanpasha2014@gmail.com>
2024-11-20 23:24:33 +0530

    Created Custom login with POST request, avoiding default Spring Security Login

--- src/main/java/com/example/weeat/controller/UserController.java
+++ src/main/java/com/example/weeat/controller/UserController.java
@@ -1,16 +1,24 @@
 package com.example.weeat.controller;
 
+import com.example.weeat.entity.User;
 import com.example.weeat.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/user")
+@RequestMapping("")
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user){
+        return userService.verify(user);
+    }
 }
--- src/main/java/com/example/weeat/service/UserService.java
+++ src/main/java/com/example/weeat/service/UserService.java
@@ -1,8 +1,12 @@
 package com.example.weeat.service;
 
+import com.example.weeat.entity.User;
 import com.example.weeat.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,4 +15,16 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    public String verify(User user) {
+        Authentication authentication =
+                authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        if(authentication.isAuthenticated()) {
+            return "Success";
+        }
+        return "Fail";
+    }
 }
